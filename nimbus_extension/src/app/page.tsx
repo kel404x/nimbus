224aa1e1afe6bede8888970707a272f7da226e80
kel404x <kel.404x@gmail.com>
2024-11-15 12:49:18 -0700

    wrapped WalletConnect with button

--- nimbus_extension/src/app/page.tsx
+++ nimbus_extension/src/app/page.tsx
@@ -3,12 +3,17 @@
 import { createWallet } from "@/service/wallet/createWallet";
 import WalletConnect from "../hooks/walletConnect";
 
-export default async function Home() {
-  await createWallet();
+
+export default function Home() {
+  const handleButtonClick = async () => {
+    await createWallet();
+  };
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <WalletConnect />
+      <button onClick={handleButtonClick}>
+        <WalletConnect />
+      </button>
     </div>
   );
 }
